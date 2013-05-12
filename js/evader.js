ab02d376da3ead6bdf2b8e530687e94e565ae17c
Isaac Zepeda <keogh24@gmail.com>
2013-05-12 00:46:24 -0600

    Obstacles fall down (first draft) and basic (test) movement for player

--- js/evader.js
+++ js/evader.js
@@ -2,12 +2,104 @@
 var Evader = function (game) {
 	window.game = game;
 
-	var player = new ze.Rect(game.canvas.width/2 - 50, game.canvas.height - 110, 100, 100);
+	var x_positions = [game.canvas.width/2 - 150, game.canvas.width/2 - 50, game.canvas.width/2 + 50];
+	var player_positions = [
+			new ze.Vec(x_positions[0], game.canvas.height - 105),
+			new ze.Vec(x_positions[1], game.canvas.height - 105),
+			new ze.Vec(x_positions[2], game.canvas.height - 105),
+		],
+		player_position = 1;
+	player = new ze.Rect(player_positions[player_position].x, player_positions[player_position].y, 100, 100)
+		.setFillStyle('#FF0000');
 	game.addObj(player);
 
-	/*app.setLoop(function () {
-		rect.pos.x += 1;
-	});*/
+	// Guide lines
+	var first_x = (game.canvas.width/2) - 50, second_x = (game.canvas.width/2) + 50;
+	game.addObj(new ze.Line(first_x, 0, first_x, game.canvas.height).setStrokeStyle('#CCCCCC'));
+	game.addObj(new ze.Line(second_x, 0, second_x, game.canvas.height).setStrokeStyle('#CCCCCC'));
+	game.addObj(new ze.Line(first_x-100, 0, first_x-100, game.canvas.height).setStrokeStyle('#CCCCCC'));
+	game.addObj(new ze.Line(second_x+100, 0, second_x+100, game.canvas.height).setStrokeStyle('#CCCCCC'));
+
+	//Events
+	var input = 0;
+	window.addEventListener('keydown', function (e) {
+		// 39 Right arrow, 37 left arrow
+		//console.log(e.keyCode);
+		if (e.keyCode == 39 && player_position < 2) {
+			player_position++;
+		} else if (e.keyCode == 37 && player_position > 0) {
+			player_position--;
+		}
+	});
+
+	// Map
+	var map = [
+		[0,0,1],
+		[0,1,0],
+		[0,1,1],
+		[1,0,0],
+		[1,0,1],
+		[1,1,0],
+	];
+
+	var obstacles = [];
+	for (var i = 0; i <= 100; i++) {
+		var ran = map[ze.getRandom(0, map.length - 1)],
+			initial_x = 0,
+			end_width = 100,
+			j = 0;
+		while (j < ran.length) {
+			if (ran[j] == 1)
+				initial_x = player_positions[j].x;
+			else {
+				j++;
+				continue;
+			}
+			if (j+1 < ran.length - 1  && ran[j+1] == 1) {
+				end_width = 200;
+				j++;
+			}
+			if (initial_x > 0)
+				obstacles.push(new ze.Rect(initial_x, -50, end_width, 20).setFillStyle('#FFCCCC'));
+			j++;
+		}
+		/*for (var j = 0; j < ran.length; j++) {
+			if (ran[j] == 1)
+				obstacles.push(new ze.Rect(player_positions[j].x, -20, 100, 20).setFillStyle('#FFCCCC'));
+		}*/
+	}
+	//console.log(obstacles);
+
+	//game.addObj(new ze.Rect(10,10,100,20).setFillStyle('#AACCBB'));
+	/*function calcSpeed(del, speed) {
+		return (speed * 60 * del) / 1000;
+	}*/
+	var current_obstacle = 0,
+		visible_obstacles = [];
+		//now, delta,
+		//then = new Date().getTime();
+	var updateObstacles = function () {
+		//now = new Date().getTime();
+		//delta = now - then;
+		if (current_obstacle == 0 || obstacles[current_obstacle-1].pos.y > 200) {
+			visible_obstacles.push(obstacles[current_obstacle]);
+			game.addObj(obstacles[current_obstacle]);
+			current_obstacle++;
+		}
+
+		for (var i = 0; i < visible_obstacles.length; i++) {
+			visible_obstacles[i].pos.y += 3;
+			//visible_obstacles[i].pos.y += calcSpeed(delta, 5);
+			if (visible_obstacles[i].pos.y > game.canvas.height) {
+				game.removeObj(visible_obstacles[i]);
+			}
+		}
+	}
+
+	game.setLoop(function () {
+		player.pos = new ze.Vec(player_positions[player_position].x, player_positions[player_position].y);
+		updateObstacles();
+	});
 }
 
 ze.init(Evader);
